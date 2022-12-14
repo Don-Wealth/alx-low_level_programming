cee41281c1a1aedd1d8b6266a8ce5221062d0baf
modozie <modoziechikeluba6@gmail.com>
2022-12-14 23:53:16 +0000

    prints the size of various types on the computer it is compiled and run on.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description: prints the size of various datatypes
+ * function - printf
+ * Result: Always print 0(succeed)
+ */
+int main(void)
+{ 
+	printf("int is    %2d bytes \n", sizeof(short int));
+	printf("long int is  %2d bytes \n", sizeof(long int));
+   	printf("float is    %2d bytes \n", sizeof(float));
+	printf("double is   %2d bytes \n", sizeof(double));
+	printf("long double is  %2d bytes \n", sizeof(long double));
+	printf("char is   %2d bytes \n", sizeof(char));
+	return 0;
+} 

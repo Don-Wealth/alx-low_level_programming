83e8a7fa6e579c5b7c3ffbf2c44ec72c0609e5e9
modozie <modoziechikeluba6@gmail.com>
2022-12-14 14:59:38 +0000

    C program that prints with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: c program that prints with printf function
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

1515476ad847c27460b5eb1b92afcf4268c0c61b
modozie <modoziechikeluba6@gmail.com>
2022-12-15 07:52:09 +0000

    prints the size of various datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu bytes \n", sizeof(char));
 	printf("Size of an int: %lu bytes \n", sizeof(int));
-	printf("Size of a double: %lu bytes \n", sizeof(double));
 	printf("Size of a long int: %lu bytes \n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes \n", sizeof(long long int));
 	printf("Size of a float: %lu bytes \n", sizeof(float));

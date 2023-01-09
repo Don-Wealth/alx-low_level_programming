fdec9724c59c9be1ee3fc663fc83254172dc2e96
Modozie <modoziechikeluba6@gmail.com>
2023-01-09 08:38:35 +0000

    initial

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ *
+ **/
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle/n");
+	return (0);
+}

5c608bf92865aeefdd08da1a81b7f198fd4d8949
modozie <modoziechikeluba6@gmail.com>
2023-01-09 10:14:11 +0000

    initial

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: a c program that prints to the stderr
+ *
+ * Return: 0 if successful
+ *
+ **/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
